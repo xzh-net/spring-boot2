ef55c1f434c186e6d47e55629cbf5c8f330610d9
xuzhihao <xcg992224@163.com>
2022-06-14 15:53:21 +0800

    删除自定义banner

--- spring-boot-security/src/test/java/net/xzh/security/SecuritySpringBootAppTests.java
+++ spring-boot-security/src/test/java/net/xzh/security/SecuritySpringBootAppTests.java
@@ -1,7 +1,8 @@
 package net.xzh.security;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,8 +11,13 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @version 1.0
  **/
 @RunWith(SpringRunner.class)
-public class TestBCrypt {
+@SpringBootTest
+public class SecuritySpringBootAppTests {
 
+	@Test
+    public void contextLoads() {
+    }
+	
     @Test
     public void testBCrypt(){
 
