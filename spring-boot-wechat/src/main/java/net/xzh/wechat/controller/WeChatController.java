6c04ebd25719d928e2e285edc5230b658c574d51
xuzhihao <xcg992224@163.com>
2022-11-02 18:06:10 +0800

    Update WeChatController.java

--- spring-boot-wechat/src/main/java/net/xzh/wechat/controller/WeChatController.java
+++ spring-boot-wechat/src/main/java/net/xzh/wechat/controller/WeChatController.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,9 +25,6 @@ import net.xzh.wechat.util.WeixinUtil;
 @RestController
 @RequestMapping("/wechat")
 public class WeChatController {
-
-	private static final Logger log = LoggerFactory.getLogger(WeChatController.class);
-
 	@Value("${wechat.wx_appid}")
 	private String wx_appid;
 	@Value("${wechat.wx_pay_key}")
@@ -47,7 +42,7 @@ public class WeChatController {
 		modelAndView.setViewName("index");
 		return modelAndView;
 	}
-	
+
 	/**
 	 * 登出
 	 * 
@@ -63,8 +58,6 @@ public class WeChatController {
 		}
 		return "forward:/wechat/index";
 	}
-	
-	
 
 	/**
 	 * 静默授权
