c265538f42b514876c2b726841d1e99cbc3533c6
xuzhihao <xcg992224@163.com>
2023-11-22 12:00:51 +0800

    Update WebSocketConfig.java

--- spring-boot-sockjs/src/main/java/net/xzh/sockjs/config/WebSocketConfig.java
+++ spring-boot-sockjs/src/main/java/net/xzh/sockjs/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	 */
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/webSocket") // 端点名称
+		registry.addEndpoint("/websocket") // 端点名称
 				// .setHandshakeHandler() 握手处理，主要是连接的时候认证获取其他数据验证等
 				// .addInterceptors() 拦截处理，和http拦截类似
 				.setAllowedOrigins("*") // 跨域
@@ -29,13 +29,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		// 这里使用的是内存模式，生产环境可以使用rabbitmq或者其他mq。
-		// 这里注册两个，主要是目的是将广播和队列分开。
-		// registry.enableStompBrokerRelay().setRelayHost().setRelayPort() 其他方式
-		registry.enableSimpleBroker("/topic/", "/queue/");
-		// 用户名称前缀p2p
-		registry.setUserDestinationPrefix("/queue/");
-		// 客户端名称前缀
+		/**
+		 * 这里使用的是内存模式，生产环境可以使用rabbitmq或者其他mq。
+		 */
+		// 定义了一个（或多个）客户端订阅地址的前缀信息，也就是客户端接收服务端发送消息的前缀信息，这里注册两个，主要是目的是将广播和点对点分开。
+		registry.enableSimpleBroker("/topic/", "/user/");
+		// 点对点使用的订阅前缀（客户端订阅路径上会体现出来），不设置的话，默认也是/user/，但是必须保证该前缀包含在客户端订阅前缀范围内
+		registry.setUserDestinationPrefix("/user/");
+		// 定义了服务端接收地址的前缀，即客户端给服务端发消息的地址前缀
 		registry.setApplicationDestinationPrefixes("/app");
+
+		// 其他方式
+		// registry.enableStompBrokerRelay().setRelayHost().setRelayPort()
 	}
 }
