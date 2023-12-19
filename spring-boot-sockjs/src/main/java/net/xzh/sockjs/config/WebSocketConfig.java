1a596fc552d37c40317a8d73bdb7a7f71b4ebc87
xuzhihao <xcg992224@163.com>
2023-12-19 20:08:12 +0800

    Update WebSocketConfig.java

--- spring-boot-sockjs/src/main/java/net/xzh/sockjs/config/WebSocketConfig.java
+++ spring-boot-sockjs/src/main/java/net/xzh/sockjs/config/WebSocketConfig.java
@@ -25,12 +25,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	}
 
 	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		// 定义了一个（或多个）客户端订阅地址的前缀信息，也就是客户端接收服务端发送消息的前缀信息，这里注册两个，主要是目的是将广播和点对点分开。
-		registry.enableSimpleBroker("/topic/", "/user/");
-		// 点对点使用的订阅前缀（客户端订阅路径上会体现出来），不设置的话，默认也是/user/，但是必须保证该前缀包含在客户端订阅前缀范围内
-		registry.setUserDestinationPrefix("/user/");
-		// 定义客户端给服务端发消息的地址前缀
-		registry.setApplicationDestinationPrefixes("/app");
-	}
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableStompBrokerRelay("/topic/","/queue/")
+                .setRelayHost("172.17.17.161")
+                .setRelayPort(61613)
+                .setClientLogin("admin")
+                .setClientPasscode("123456")
+                .setVirtualHost("/");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
 }
