6fe2ca2fdf069db6213561cd9953b7b505219cc1
13998417419 <xcg992224@163.com>
2024-03-13 17:26:01 +0800

    tomcat调优

---
+++ spring-boot-pg-jmeter/README.md
@@ -0,0 +1,29 @@
+# pg百万性能查询调优
+
+
+## 1. 创建表
+
+```sql
+drop table test;
+create table test as (
+	select
+		generate_series ( 1, 1000000 ) ID,
+		date ( generate_series ( now( ), now( ) + '52 week', '100 day' ) ) create_time,
+		substring ( md5( random( ) :: varchar ), 2, 8 ) :: varchar vid,
+	( random( ) * 10000 ) :: numeric ( 5 ) play 
+	);
+```
+
+创建索引
+```bash
+create index test_idx ON test ("id");
+```
+
+## 2. 测试结果
+
+![](dashboard.png)
+
+```bash
+mvn clean compile
+mvn clean package
+```
---
+++ spring-boot-pg-jmeter/src/main/java/net/xzh/jmeter/controller/TestController.java
@@ -0,0 +1,29 @@
+package net.xzh.jmeter.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * pg百万性能查询调优
+ * 
+ * @author xzh
+ *
+ */
+@RestController
+public class TestController {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@GetMapping("/")
+	public List<Map<String, Object>> list(Long id) {
+		List<Map<String, Object>> queryForList = jdbcTemplate.queryForList("select * from test where id = ?",id);
+		return queryForList;
+	}
+
+}
