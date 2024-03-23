9ee2e1ef50deac8dfdbee89df2f7ed3bddeb1e33
13998417419 <xcg992224@163.com>
2024-03-23 14:03:31 +0800

    update

--- spring-boot-security/src/main/java/net/xzh/security/Application.java
+++ spring-boot-security/src/main/java/net/xzh/security/Application.java
@@ -3,10 +3,6 @@ package net.xzh.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author Administrator
- * @version 1.0
- **/
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
