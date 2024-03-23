44c956af848bf3292f730b4fc4c69080679abffc
13998417419 <xcg992224@163.com>
2024-03-23 15:49:50 +0800

    update

--- spring-boot-sharding-jdbc/src/test/java/net/xzh/sharding/ShardingApplicationTests.java
+++ spring-boot-sharding-jdbc/src/test/java/net/xzh/sharding/ShardingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.macro.mall.tiny;
+package net.xzh.sharding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class MallTinyApplicationTests {
+public class ShardingApplicationTests {
 
     @Test
     public void contextLoads() {
