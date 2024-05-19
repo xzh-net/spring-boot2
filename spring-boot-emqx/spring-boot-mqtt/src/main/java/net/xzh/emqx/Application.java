268a9acd333cb1145f2d16cbf59922df81adb3e4
xzh-net <xcg992224@163.com>
2024-05-19 20:18:35 +0800

    Update Application.java

--- spring-boot-emqx/spring-boot-mqtt/src/main/java/net/xzh/emqx/Application.java
+++ spring-boot-emqx/spring-boot-mqtt/src/main/java/net/xzh/emqx/Application.java
@@ -2,9 +2,8 @@ package net.xzh.emqx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//排除自动配置
+@SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
