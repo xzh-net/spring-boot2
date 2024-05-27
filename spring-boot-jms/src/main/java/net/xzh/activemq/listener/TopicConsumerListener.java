b99772d649e0db9fc4849704dc78ec1c596dce17
13998417419 <xcg992224@163.com>
2024-05-27 17:47:19 +0800

    Update TopicConsumerListener.java

--- spring-boot-jms/src/main/java/net/xzh/activemq/listener/TopicConsumerListener.java
+++ spring-boot-jms/src/main/java/net/xzh/activemq/listener/TopicConsumerListener.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Component;
 
 /**
  * topic模式的消费者
+ * 
  * @author CR7
  *
  */
 @Component
 public class TopicConsumerListener {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(TopicConsumerListener.class);
 
-	 @JmsListener(destination = "topic", containerFactory = "jmsListenerContainerTopic")
-     public void receive(String msg) {
-		 log.info("topic接受到，{}",msg);
-     }
+	@JmsListener(destination = "topic", containerFactory = "jmsListenerContainerTopic")
+	public void receive(String msg) {
+		log.info("topic接受到，{}", msg);
+	}
 }
