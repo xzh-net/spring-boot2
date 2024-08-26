fd4dc3506ab2c27ebdf34a48a05f02a1d3611cdb
13998417419 <xcg992224@163.com>
2024-08-26 18:22:35 +0800

    Update DeploymentController.java

--- spring-boot-k8s/spring-boot-k8s-client/src/main/java/net/xzh/k8s/controller/DeploymentController.java
+++ spring-boot-k8s/spring-boot-k8s-client/src/main/java/net/xzh/k8s/controller/DeploymentController.java
@@ -1,11 +1,13 @@
 package net.xzh.k8s.controller;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.kubernetes.client.openapi.ApiException;
@@ -13,11 +15,13 @@ import io.kubernetes.client.openapi.apis.AppsV1Api;
 import io.kubernetes.client.openapi.models.V1Container;
 import io.kubernetes.client.openapi.models.V1Deployment;
 import io.kubernetes.client.openapi.models.V1DeploymentBuilder;
+import io.kubernetes.client.openapi.models.V1DeploymentList;
 import io.kubernetes.client.openapi.models.V1DeploymentSpec;
 import io.kubernetes.client.openapi.models.V1LabelSelector;
 import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import io.kubernetes.client.openapi.models.V1PodSpec;
 import io.kubernetes.client.openapi.models.V1PodTemplateSpec;
+import io.kubernetes.client.openapi.models.V1Status;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import net.xzh.k8s.common.model.CommonResult;
@@ -37,11 +41,10 @@ public class DeploymentController {
 
 	@ApiOperation("创建Deployment")
 	@RequestMapping(value = "/createDeployment", method = RequestMethod.GET)
-	public CommonResult<?> createDeployments() {
+	public CommonResult<?> createDeployments(@RequestParam String namespace) {
 		AppsV1Api apiInstance = new AppsV1Api();
-		String deploymentName = "example-nginx";
+		String deploymentName = "test-deploy-nginx";
 	    String imageName = "nginx:1.22.1";
-	    String namespace = "test6";
 
 	    // Create an example deployment
 	    V1DeploymentBuilder deploymentBuilder =
@@ -72,6 +75,37 @@ public class DeploymentController {
 			e.printStackTrace();
 		}
 		return CommonResult.success(v1Deployment.getMetadata().getName());
+	}
+	
+	
+	@ApiOperation("删除Deployment")
+	@RequestMapping(value = "/deleteDeployment", method = RequestMethod.GET)
+	public CommonResult<?> deleteDeployment(@RequestParam String namespace,@RequestParam String deployName) {
+		AppsV1Api apiInstance = new AppsV1Api();
+		V1Status v1Status =  null;
+	    try {
+	    	v1Status = apiInstance.deleteNamespacedDeployment(deployName,namespace, null, null, null, null, null, null);
+		} catch (ApiException e) {
+			logger.error("Exception when calling AppsV1Api#deleteNamespacedDeployment");
+			e.printStackTrace();
+		}
+		return CommonResult.success(v1Status);
+	}
+	
+	@ApiOperation("按命名空间查询所有Deployment")
+	@RequestMapping(value = "/listDeployment", method = RequestMethod.GET)
+	public CommonResult<?> listDeployment(@RequestParam String namespace) {
+		AppsV1Api apiInstance = new AppsV1Api();
+		ArrayList<String> list = new ArrayList<String>();
+		V1DeploymentList deploymentList =  null;
+	    try {
+	    	 deploymentList  = apiInstance.listNamespacedDeployment(namespace, null, null, null, null, null, null, null, null, null, null);
+	    	 deploymentList.getItems().forEach(deployment -> list.add(deployment.getMetadata().getName()));
+		} catch (ApiException e) {
+			logger.error("Exception when calling AppsV1Api#listNamespacedDeployment");
+			e.printStackTrace();
+		}
+		return CommonResult.success(list);
 	}
 
 
