0fbb07519ca29477bdcd9647d3925f15923959d0
13998417419 <xcg992224@163.com>
2024-09-26 09:28:36 +0800

    Update NovaController.java

--- spring-boot-openstack/spring-boot-jclouds/src/main/java/net/xzh/jclouds/controller/NovaController.java
+++ spring-boot-openstack/spring-boot-jclouds/src/main/java/net/xzh/jclouds/controller/NovaController.java
@@ -1,6 +1,5 @@
 package net.xzh.jclouds.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
