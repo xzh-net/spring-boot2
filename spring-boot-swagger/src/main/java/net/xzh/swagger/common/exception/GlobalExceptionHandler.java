9c7ff83f7eb9d9655a8593559561f92c21df4459
13998417419 <xcg992224@163.com>
2024-09-27 17:18:42 +0800

    Update GlobalExceptionHandler.java

--- spring-boot-swagger/src/main/java/net/xzh/swagger/common/exception/GlobalExceptionHandler.java
+++ spring-boot-swagger/src/main/java/net/xzh/swagger/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package net.xzh.swagger.common.exception;
 
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +23,20 @@ public class GlobalExceptionHandler {
             return CommonResult.failed(e.getErrorCode());
         }
         return CommonResult.failed(e.getMessage());
+    }
+    
+    @ResponseBody
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public CommonResult<?> handleValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String message = null;
+        if (bindingResult.hasErrors()) {
+            FieldError fieldError = bindingResult.getFieldError();
+            if (fieldError != null) {
+                message = fieldError.getField()+fieldError.getDefaultMessage();
+            }
+        }
+        return CommonResult.validateFailed(message);
     }
 }
 
