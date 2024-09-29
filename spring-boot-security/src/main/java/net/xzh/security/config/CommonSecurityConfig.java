5f686ed9f707e55e718bf3a1ac0df32b6187ffc5
13998417419 <xcg992224@163.com>
2024-09-29 15:55:43 +0800

    Update CommonSecurityConfig.java

--- spring-boot-security/src/main/java/net/xzh/security/config/CommonSecurityConfig.java
+++ spring-boot-security/src/main/java/net/xzh/security/config/CommonSecurityConfig.java
@@ -3,7 +3,6 @@ package net.xzh.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import net.xzh.security.common.util.JwtTokenUtil;
