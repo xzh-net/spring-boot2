629928a6fbab51cabd3fb0ada115622138a46b18
13998417419 <xcg992224@163.com>
2024-09-29 15:53:42 +0800

    Create PwdEncoderUtil.java

---
+++ spring-boot-security/src/main/java/net/xzh/security/common/util/PwdEncoderUtil.java
@@ -0,0 +1,28 @@
+package net.xzh.security.common.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.Assert;
+
+import net.xzh.security.common.passwordEncoder.SM3PasswordEncoder;
+
+/**
+ * PasswordEncoder实现工具类
+ *
+ * add by xzh 2024/09/29
+ */
+public class PwdEncoderUtil {
+    public static PasswordEncoder getDelegatingPasswordEncoder(String encodingId) {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("SM3", new SM3PasswordEncoder());
+        Assert.isTrue(encoders.containsKey(encodingId), encodingId + " is not found in idToPasswordEncoder");
+        DelegatingPasswordEncoder delegatingPasswordEncoder = new DelegatingPasswordEncoder(encodingId, encoders);
+        delegatingPasswordEncoder.setDefaultPasswordEncoderForMatches(encoders.get(encodingId));
+        return delegatingPasswordEncoder;
+    }
+}
