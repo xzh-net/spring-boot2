72c4dc3ae00d4f14604405d490f97ba7b5591021
xzh <xcg992224@163.com>
2025-03-21 16:47:41 +0800

    add zipkin

---
+++ spring-boot-zipkin/README.md
@@ -0,0 +1,18 @@
+# Zipkin 实现分布式全链路监控
+
+
+## 1.获取镜像
+
+```shell
+docker pull openzipkin/zipkin
+```
+
+## 2.启动
+
+```shell
+docker run --name zipkin -d -p 9411:9411 openzipkin/zipkin
+```
+
+## 3.web端查看
+
+> 浏览器打开 http://127.0.0.1:9411
---
+++ spring-boot-zipkin/src/main/java/net/xzh/zipkin/controller/ZipkinController.java
@@ -0,0 +1,56 @@
+package net.xzh.zipkin.controller;
+
+import java.net.URI;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+@RestController
+public class ZipkinController {
+    private static Logger log = LoggerFactory.getLogger(ZipkinController.class);
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("ping")
+    public Object ping() {
+        log.info("进入ping");
+        return "pong study";
+    }
+
+    @RequestMapping("log")
+    public Object log() {
+        log.info("this is info log");
+        log.error("this is error log");
+        log.debug("this is debug log");
+        log.warn("this is warn log");
+        log.trace("this is trace log");
+        return "123";
+    }
+
+    @RequestMapping("http")
+    public Object httpQuery() {
+
+        String studyUrl = "http://localhost:8080/ping";
+        URI studyUri = URI.create(studyUrl);
+        String study = restTemplate.getForObject(studyUri, String.class);
+        log.info("study:{}", study);
+
+        String floorUrl = "http://localhost:8080/log";
+        URI floorUri = URI.create(floorUrl);
+        String floor = restTemplate.getForObject(floorUri, String.class);
+        log.info("floor:{}", floor);
+
+        String roomUrl = "http://localhost:8080/ping";
+        URI roomUri = URI.create(roomUrl);
+        String room = restTemplate.getForObject(roomUri, String.class);
+        log.info("room:{}", room);
+        return study + "-------" + floor + "-------" + room + "-------";
+    }
+
+
+}
