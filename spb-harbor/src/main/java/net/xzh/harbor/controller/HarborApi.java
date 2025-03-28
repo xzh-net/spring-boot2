c9c98b4beee8c8d072bc6835d001a9da2af39db9
xzh <xcg992224@163.com>
2025-03-28 22:03:52 +0800

    Update HarborApi.java

--- spb-harbor/src/main/java/net/xzh/harbor/controller/HarborApi.java
+++ spb-harbor/src/main/java/net/xzh/harbor/controller/HarborApi.java
@@ -2,26 +2,12 @@ package net.xzh.harbor.controller;
 
 import java.util.Base64;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import cn.hutool.json.JSONUtil;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import net.xzh.harbor.common.model.CommonResult;
-import net.xzh.harbor.model.HarborUser;
 
 /**
  * Harbor管理
@@ -31,87 +17,89 @@ import net.xzh.harbor.model.HarborUser;
  */
 public class HarborApi {
 
-	
-//	harbor:
-//		   username: admin
-//		   password: LNyD2h9HSr7oZaqI
-//		   url: http://172.17.17.37:8088/api/v2.0
-			   
-			   
-	@Value("${harbor.username}")
-	private String harborAdminUsername;
-	@Value("${harbor.password}")
-	private String harborAdminPassword;
-	@Value("${harbor.url}")
-	private String harborUrl;
-
-	@Autowired
-	RestTemplate restTemplate;
-
-	@ApiOperation("创建账号")
-	@RequestMapping(value = "/createUser", method = RequestMethod.POST)
-	public CommonResult<?> createUser(@RequestBody HarborUser harborUser) {
-		// 设置header和认证
-		HttpHeaders headers = createHeaders(harborAdminUsername, harborAdminPassword);
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.add("Accept", "application/json");
-		headers.add("Content-Type", "application/json");
-		HttpEntity<String> entity = new HttpEntity<>(JSONUtil.toJsonStr(harborUser), headers);
-		ResponseEntity<String> resp = restTemplate.exchange(harborUrl + "/users", HttpMethod.POST, entity,
-				new ParameterizedTypeReference<String>() {
-				});
-		return CommonResult.success(resp);
+	private HarborApi() {
 
 	}
 
-	@ApiOperation("查询当前账号")
-	@RequestMapping(value = "/getcurrentUser", method = RequestMethod.GET)
-	public CommonResult<?> getcurrentUser(@RequestParam String username, @RequestParam String password) {
-		// 设置header和认证
-		HttpHeaders headers = createHeaders(username, password);
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.add("Accept", "application/json");
-		HttpEntity<String> entity = new HttpEntity<>(headers);
-		HttpEntity<String> resp = restTemplate.exchange(harborUrl + "/users/current", HttpMethod.GET, entity,
-				String.class);
-		return CommonResult.success(JSONUtil.parseObj(resp.getBody()));
-	}
-
-	@ApiOperation("删除账号")
-	@RequestMapping(value = "/deleteUser", method = RequestMethod.DELETE)
-	public CommonResult<?> deleteUser(@RequestParam String userId) {
-		HttpHeaders headers = createHeaders(harborAdminUsername, harborAdminPassword);
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.add("Accept", "application/json");
-		HttpEntity<String> entity = new HttpEntity<>(headers);
-		restTemplate.exchange(harborUrl + "/users/" + userId, HttpMethod.DELETE, entity, String.class);
-		return CommonResult.success(System.currentTimeMillis());
-	}
-
-	@ApiOperation("查询所有账号")
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public CommonResult<?> list() {
-		HttpHeaders headers = createHeaders(harborAdminUsername, harborAdminPassword);
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		headers.add("Accept", "application/json");
-		HttpEntity<String> entity = new HttpEntity<>(headers);
-		HttpEntity<String> resp = restTemplate.exchange(harborUrl + "/users", HttpMethod.GET, entity, String.class);
-		return CommonResult.success(resp.getBody());
-	}
+	// 连接 Harbor 需要设置的信息
+	private static String harborUrl = "http://172.17.17.37:8088/api/v2.0";
+	private static String harborUsername = "admin";
+	private static String harborPassword = "LNyD2h9HSr7oZaqI";
 
 	/**
 	 * Authorization Basic认证
 	 * 
+	 * @param username
+	 * @param password
 	 * @return
 	 */
 	@SuppressWarnings("serial")
-	public static HttpHeaders createHeaders(String username, String password) {
+	private HttpHeaders createHeaders(String username, String password) {
 		return new HttpHeaders() {
 			{
 				String auth = username + ":" + password;
 				String authHeader = "Basic " + Base64.getEncoder().encodeToString(auth.getBytes());
 				set("Authorization", authHeader);
+				setContentType(MediaType.APPLICATION_JSON);
 			}
 		};
 	}
+
+	/**
+	 * 创建用户,密码必须满足复杂度
+	 */
+	public void createUser() {
+		String user = String.format("{\"comment\": \"\"," + "\"email\": \"xuzhihao@163.com\","
+				+ "\"password\": \"123qWE!@#\"," + "\"realname\": \"徐智豪\"," + "\"username\": \"xuzhihao\"}");
+		HttpEntity<String> entity = new HttpEntity<>(user, createHeaders(harborUsername, harborPassword));
+		ResponseEntity<String> resp = new RestTemplate().exchange(harborUrl + "/users", HttpMethod.POST, entity,
+				String.class);
+		System.out.println(resp.getBody());
+	}
+
+	/**
+	 * 查询用户是否存在
+	 * 
+	 * @param username
+	 * @param password
+	 */
+	public void users(String username, String password) {
+		HttpEntity<String> entity = new HttpEntity<>(createHeaders(username, password));
+		HttpEntity<String> resp = new RestTemplate().exchange(harborUrl + "/users/current", HttpMethod.GET, entity,
+				String.class);
+		System.out.println(resp.getBody());
+	}
+	
+	/**
+	 * 删除用户
+	 * @param userId
+	 */
+	public void removeUser(String userId) {
+		HttpEntity<String> entity = new HttpEntity<>(createHeaders(harborUsername, harborPassword));
+		HttpEntity<String> resp=new RestTemplate().exchange(harborUrl + "/users/" + userId, HttpMethod.DELETE, entity, String.class);
+		System.out.println(resp.getBody());
+	}
+	
+	/**
+	 * 查询所有用户
+	 */
+	public void listUser() {
+		HttpEntity<String> entity = new HttpEntity<>(createHeaders(harborUsername, harborPassword));
+		HttpEntity<String> resp=new RestTemplate().exchange(harborUrl + "/users", HttpMethod.GET, entity, String.class);
+		System.out.println(resp.getBody());
+	}
+
+	public static void main(String[] args) {
+		HarborApi api = new HarborApi();
+		// 创建用户
+//		api.createUser();
+		// 查询用户
+//		api.users("xuzhihao", "123qWE!@#");
+		// 删除用户
+//		api.removeUser("37");
+		//查询所有用户
+		api.listUser();
+
+	}
+
 }
