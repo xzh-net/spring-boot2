7c2d128f52ee6d87cdb320215c57135917e5827d
xzh <xcg992224@163.com>
2025-07-11 17:23:01 +0800

    Update LoginHandlerInterceptor.java

--- spring-boot-stand/src/main/java/net/xzh/Interceptor/LoginHandlerInterceptor.java
+++ spring-boot-stand/src/main/java/net/xzh/Interceptor/LoginHandlerInterceptor.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import cn.hutool.core.util.StrUtil;
@@ -30,7 +31,11 @@ public class LoginHandlerInterceptor implements HandlerInterceptor {
 		String requestUri = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		String url = requestUri.substring(contextPath.length());
-		if (!authProperties.getNoAuthUrls().contains(url)) {// 需要拦截认证的页面
+		// 使用AntPathMatcher进行路径匹配
+	    AntPathMatcher pathMatcher = new AntPathMatcher();
+	    boolean isNoAuth = authProperties.getNoAuthUrls().stream()
+	            .anyMatch(pattern -> pathMatcher.match(pattern, url));
+		if (isNoAuth) {// 需要拦截认证的页面
 			String loginName = (String) request.getSession().getAttribute("user");
 			if (StrUtil.isBlank(loginName)) {
 				response.sendRedirect(request.getContextPath() + "/"); // 未登录自动跳转界面
