e8237f1935085c6f78dd18ff67d59af43ff8b3bc
xzh <xcg992224@163.com>
2025-07-11 16:23:08 +0800

    Update User.java

--- spring-boot-stand/src/main/java/net/xzh/model/User.java
+++ spring-boot-stand/src/main/java/net/xzh/model/User.java
@@ -1,17 +1,32 @@
 package net.xzh.model;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
-
-import lombok.Data;
-
-@Data  // Lombok注解
-@TableName("user")  // 指定表名
 public class User {
-    @TableId(type = IdType.AUTO)  // 主键自增
-    private Long id;
-    private String name;
-    private Integer age;
-    private String email;
+	private Long id;
+	private String name;
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
