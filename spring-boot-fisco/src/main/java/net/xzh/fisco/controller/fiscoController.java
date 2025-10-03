3a2b6d6ee788decf21e3af5fdfaaf0303ecc6b78
xzh <xcg992224@163.com>
2025-10-03 15:36:45 +0800

    update

--- spring-boot-fisco/src/main/java/net/xzh/fisco/controller/fiscoController.java
+++ spring-boot-fisco/src/main/java/net/xzh/fisco/controller/fiscoController.java
@@ -8,22 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import net.xzh.fisco.config.FiscoBcos;
 
 
 /**
  * 配置管理
  */
-@Tag(name = "令牌管理", description = "配置管理相关的API")
 @RestController
 public class fiscoController {
 
 	@Autowired
 	private FiscoBcos fiscoBcos;
 
-	@Operation(summary = "查询区块高度", description = "查询区块高度")
+	/**
+	 * 查询区块高度
+	 * @return
+	 */
 	@RequestMapping(value = "/block", method = RequestMethod.GET)
 	public Object Number() {
 		BcosSDK bcosSDK = fiscoBcos.getBcosSDK();
