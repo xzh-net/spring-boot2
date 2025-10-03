876b0e414325a4b0e4a15e23c3c8c3f5b98b3d54
xzh <xcg992224@163.com>
2025-10-03 15:59:24 +0800

    springdoc 1.7.0 springboot 2.7.0

---
+++ spring-boot-springdoc/src/main/java/net/xzh/springdoc/SpringDocApplication.java
@@ -0,0 +1,13 @@
+package net.xzh.springdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDocApplication.class, args);
+    }
+
+}
