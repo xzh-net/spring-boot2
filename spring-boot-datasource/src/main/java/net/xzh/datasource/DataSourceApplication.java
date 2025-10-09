5d6d3721cec3f34cc6f46b65d090c23999022405
xzh <xcg992224@163.com>
2025-10-09 10:20:52 +0800

    update

--- spring-boot-datasource/src/main/java/net/xzh/datasource/DataSourceApplication.java
+++ spring-boot-datasource/src/main/java/net/xzh/datasource/DataSourceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DynamicDataSourceApplication {
+public class DataSourceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DynamicDataSourceApplication.class, args);
+		SpringApplication.run(DataSourceApplication.class, args);
 	}
 }
