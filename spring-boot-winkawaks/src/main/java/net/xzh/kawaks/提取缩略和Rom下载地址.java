b2b3d540d41de4ee941cc31c8f39f18af465f2af
xzh <xcg992224@163.com>
2025-10-10 19:43:36 +0800

    Create 提取缩略和Rom下载地址.java

---
+++ spring-boot-winkawaks/src/main/java/net/xzh/kawaks/提取缩略和Rom下载地址.java
@@ -0,0 +1,41 @@
+package net.xzh.kawaks;
+
+import java.io.File;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.CharsetUtil;
+
+/**
+ * 第3步
+ * @author CR7
+ *
+ */
+public class 提取缩略和Rom下载地址 {
+	public static void main(String[] args) {
+		getAllFiles(new File("C://Users//CR7//Desktop//winkawaks//roms//cps1//roms"));
+//		getAllFiles(new File("C://Users//CR7//Desktop//winkawaks//roms//cps2//roms"));
+//		getAllFiles(new File("C://Users//CR7//Desktop//winkawaks//roms//neogeo//roms"));
+	}
+
+	private static void getAllFiles(File dir) {
+		File[] files = dir.listFiles();
+		if (files != null) {
+			for (File file : files) {
+				if (file.isDirectory()) {
+					getAllFiles(file);
+				} else {
+					String content = FileUtil.readString(file, CharsetUtil.charset("UTF-8"));
+					Document document = Jsoup.parse(content);
+					String img = document.select("#rom-screen img").attr("src");
+					System.out.println("https://www.kawaks.org" + img);
+			    	String href = document.select("#rom-url .rom-value a").attr("href");
+					System.out.println(href);
+				}
+			}
+		}
+	}
+
+}
