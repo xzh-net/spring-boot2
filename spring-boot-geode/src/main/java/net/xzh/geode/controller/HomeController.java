30356a2478e2aee81b2bfef2b78e7a79fb1313f3
xzh <xcg992224@163.com>
2025-10-24 13:47:00 +0800

    update

--- spring-boot-geode/src/main/java/net/xzh/geode/controller/HomeController.java
+++ spring-boot-geode/src/main/java/net/xzh/geode/controller/HomeController.java
@@ -21,26 +21,44 @@ public class HomeController {
 	@Autowired
 	UserRepository userRepository;
 
-	@RequestMapping(value = "/insert", method = RequestMethod.POST)
-	public Object insert(@RequestBody User user) {
+	/**
+	 * 插入用户
+	 * @param user
+	 * @return
+	 */
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public Object add(@RequestBody User user) {
 		userRepository.save(user);
-		return "add OK";
+		return "创建成功";
 	}
 
-	@RequestMapping(value = "/findAll", method = RequestMethod.GET)
-	public Object findAll() {
+	/**
+	 * 查询全部
+	 * @return
+	 */
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public Object list() {
 		return userRepository.findAll();
 	}
-
+	/**
+	 * 按用户id查询
+	 * @param id
+	 * @return
+	 */
 	@RequestMapping(value = "/findById", method = RequestMethod.GET)
 	public Object findById(String id) {
 		return userRepository.findById(id);
 	}
 
-	@RequestMapping(value = "/deleteById", method = RequestMethod.DELETE)
-	public Object deleteById(String id) {
+	/**
+	 * 删除用户
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/removeById", method = RequestMethod.DELETE)
+	public Object removeById(String id) {
 		userRepository.deleteById(id);
-		return "delete OK";
+		return "删除成功";
 
 	}
 }
