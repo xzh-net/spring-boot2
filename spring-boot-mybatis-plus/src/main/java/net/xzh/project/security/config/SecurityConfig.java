00a2f473b9cc625e528d24bcccdf2fe999b4c6ec
xzh <xcg992224@163.com>
2025-10-25 11:07:22 +0800

    Update SecurityConfig.java

--- spring-boot-mybatis-plus/src/main/java/net/xzh/project/security/config/SecurityConfig.java
+++ spring-boot-mybatis-plus/src/main/java/net/xzh/project/security/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
@@ -60,28 +59,28 @@ public class SecurityConfig {
 	private DynamicSecurityFilter dynamicSecurityFilter;
 
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-		ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = httpSecurity
-				.authorizeRequests();
-		// 不需要保护的资源路径允许访问
-		for (String url : ignoreUrlsConfig.getUrls()) {
-			registry.antMatchers(url).permitAll();
-		}
-		// 允许跨域请求的OPTIONS请求
-		registry.antMatchers(HttpMethod.OPTIONS).permitAll();
-		// 任何请求需要身份认证
-		registry.and().authorizeRequests().anyRequest().authenticated()
-				// 关闭跨站请求防护及不使用session
-				.and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				// 自定义权限拒绝处理类
-				.and().exceptionHandling().accessDeniedHandler(restfulAccessDeniedHandler)
-				.authenticationEntryPoint(restAuthenticationEntryPoint)
-				// 自定义权限拦截器JWT过滤器
-				.and().addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
-		// 有动态权限配置时添加动态权限校验过滤器
-		if (dynamicSecurityService != null) {
-			registry.and().addFilterBefore(dynamicSecurityFilter, FilterSecurityInterceptor.class);
-		}
-		return httpSecurity.build();
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    HttpSecurity httpSecurity = http
+	        .authorizeHttpRequests(authz -> authz
+	        	.antMatchers(HttpMethod.OPTIONS).permitAll()
+	            .antMatchers(ignoreUrlsConfig.getUrls().toArray(new String[0])).permitAll()
+	            .anyRequest().authenticated()
+	        )
+	        .csrf(csrf -> csrf.disable())
+	        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	        .exceptionHandling(exception -> exception
+	        	// 自定义权限拒绝处理类
+	            .accessDeniedHandler(restfulAccessDeniedHandler)
+	            // 自定义认证拒绝处理类
+	            .authenticationEntryPoint(restAuthenticationEntryPoint)
+	        )
+	        // 自定义权限拦截器JWT过滤器
+	        .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+	    
+	    // 有动态权限配置时添加动态权限校验过滤器
+	    if (dynamicSecurityService != null) {
+	        httpSecurity.addFilterBefore(dynamicSecurityFilter, FilterSecurityInterceptor.class);
+	    }
+	    return httpSecurity.build();
 	}
 }
