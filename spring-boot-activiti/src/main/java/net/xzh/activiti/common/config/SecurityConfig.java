a485587cdc326580ba05896830ee1de4b271abf0
xzh <xcg992224@163.com>
2025-10-25 10:44:04 +0800

    update

--- spring-boot-activiti/src/main/java/net/xzh/activiti/common/config/SecurityConfig.java
+++ spring-boot-activiti/src/main/java/net/xzh/activiti/common/config/SecurityConfig.java
@@ -3,14 +3,17 @@ package net.xzh.activiti.common.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import net.xzh.activiti.common.component.JwtAuthenticationTokenFilter;
+import net.xzh.activiti.common.config.properties.IgnoreUrlsProperties;
 
 /**
  * 安全设置
+ * 
  * @author xzh
  *
  */
@@ -19,20 +22,22 @@ public class SecurityConfig {
 
 	@Autowired
 	private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
-	
-	
+
+	@Autowired
+	private IgnoreUrlsProperties ignoreUrlsProperties;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(authz -> authz
-				.antMatchers("/login", "/captcha", "/css/**", "/js/**", "/images/**", "/lib/**", "/webjars/**")
-				.permitAll().anyRequest().authenticated()).formLogin(form -> form.loginPage("/index") // 自定义登录页面
+				// 允许跨域请求的OPTIONS请求
+				.antMatchers(HttpMethod.OPTIONS).permitAll()
+				.antMatchers(ignoreUrlsProperties.getUrls().toArray(new String[0])).permitAll().anyRequest()
+				.authenticated()).formLogin(form -> form.loginPage("/index") // 自定义登录页面
 						.permitAll())
 				.logout(logout -> logout.permitAll()).csrf().disable()
-				.headers(headers -> headers
-			            .frameOptions().sameOrigin() // 允许同源 iframe
-			        )
-				.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+				.headers(headers -> headers.frameOptions().sameOrigin() // 允许同源 iframe
+				).addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
-	
+
 }
