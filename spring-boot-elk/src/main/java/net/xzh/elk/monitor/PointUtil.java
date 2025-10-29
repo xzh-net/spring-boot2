25a494a19544d899690b639c329119baf364c39e
xzh <xcg992224@163.com>
2025-10-29 15:53:37 +0800

    Update PointUtil.java

--- spring-boot-elk/src/main/java/net/xzh/elk/monitor/PointUtil.java
+++ spring-boot-elk/src/main/java/net/xzh/elk/monitor/PointUtil.java
@@ -2,8 +2,6 @@ package net.xzh.elk.monitor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 import org.springframework.stereotype.Component;
 
 /**
